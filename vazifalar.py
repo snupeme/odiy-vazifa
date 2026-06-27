bf24165f16c416c0534a16086d691993dccda44f
Abdulaziz <github-emailing@example.com>
2026-06-27 13:16:47 +0300

    Birinchi loyiha: kalkulyator

---
+++ vazifalar.py
@@ -0,0 +1,12 @@
+print("=====kankulyator=====")
+son1 = int(input("birinchi soni kiriting: "))
+son2 = int(input("ikkinchi soni kiriting: "))
+print(f"{son1} + {son2} = {son1 + son2}")
+print(f"{son1} - {son2} = {son1 - son2}")
+print(f"{son1} * {son2} = {son1 * son2}")
+print(f"{son1} / {son2} = {son1 / son2}")
+print(f"{son1} // {son2} = {son1 // son2}")
+print(f"{son1} % {son2} = {son1 % son2}")
+print(f"{son1} ** {son2} = {son1 ** son2}")
+
+
